971f79c0235f7e91077158504a806fff27b1aa4f
DmitriyVolchenko <Dmitriy.Volchenko@gmail.com>
2024-03-12 17:57:09 +0200

    start commit

---
+++ src/telran/util/ArraysInt.java
@@ -0,0 +1,22 @@
+package telran.util;
+
+public class ArraysInt {
+	
+	public static int[] addNumber(int[] array, int number) {
+		//TODO apply method copyOf class Arrays
+		return null;
+	}
+	public static int[] insertNumber(int[] array,int index,  int number) {
+		//TODO apply method arraycopy of class System
+		return null;
+	}
+	public static int[] removeNumber(int[] array,int index) {
+		//TODO TODO apply method arraycopy of class System
+		return null;
+	}
+	public static int[] insertSorted(int[] sortedArray,int number) {
+		//TODO insert number into sorted array with keeping the sorted order
+//apply method binerySearch of the class Arrays
+		return null;
+	}
+}
---
+++ src/telran/util/tests/ArraysTest.java
@@ -0,0 +1,96 @@
+package telran.util.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import static telran.util.ArraysInt.*;
+
+import java.util.Arrays;
+
+
+
+
+
+class ArraysTest {
+	int [] array = {10, 25, 30, 40, 100};
+	
+	
+	@Test
+	void addNumberTest() {
+		int[] arrayExpected = {10, 25, 30, 40, 100, -2};
+		
+		int[] arrayActual = addNumber(array, -2);
+
+		assertArrayEquals(arrayExpected, arrayActual);
+	}
+	@Test
+	void insertNumberText() {
+		int[] arrayExpectedLast = {10, 25, 30, 40, 100, 25};
+		int[] arrayExpectedFirst = { 25, 10, 25, 30, 40, 100};
+		int[] arrayExpectedMiddle = { 10, 25, 30, 25, 40, 100};
+		int[] arrayActualLast = insertNumber(array, 5, 25);
+		int[] arrayActualFirst = insertNumber(array,0, 25);
+		int[] arrayActualMiddle = insertNumber(array,3, 25);
+		
+		assertArrayEquals (arrayExpectedLast, arrayActualLast);
+		assertArrayEquals (arrayExpectedFirst, arrayActualFirst);
+		assertArrayEquals (arrayExpectedMiddle, arrayActualMiddle);
+	}
+	@Test
+	void removeNumberText() {
+		int[] arrayExpectedLast = {10, 25, 30, 40};
+		int[] arrayExpectedFirst = { 25, 30, 40, 100};
+		int[] arrayExpectedMiddle = { 10, 25, 40, 100};
+		int[] arrayActualLast = removeNumber(array,array.length-1);
+		int[] arrayActualFirst = removeNumber(array,0);
+		int[] arrayActualMiddle = removeNumber(array,2);
+	
+		assertArrayEquals (arrayExpectedLast, arrayActualLast);
+		assertArrayEquals (arrayExpectedFirst, arrayActualFirst);
+		assertArrayEquals (arrayExpectedMiddle, arrayActualMiddle);
+	
+	}
+	@Test
+	void insertSortedText() {
+		int[] arrayExpectedLast = {10, 25, 30, 40, 100, 150};
+		int[] arrayExpectedFirst = { 10,10, 25, 30, 40, 100};
+		int[] arrayExpectedMiddle = { 10, 25, 30, 35, 40, 100};
+		int[] arrayActualLast = insertSorted(array, 150);
+		int[] arrayActualFirst = insertSorted(array,10);
+		int[] arrayActualMiddle = insertSorted(array,35);
+		assertArrayEquals (arrayExpectedLast, arrayActualLast);
+		assertArrayEquals (arrayExpectedFirst, arrayActualFirst);
+		assertArrayEquals (arrayExpectedMiddle, arrayActualMiddle);
+	
+	}
+	
+	@Test
+	void copyOfTest() {
+		int[] arrayExpectedSameLength = {10, 25, 30, 40, 100};
+		int[] arrayExpectedLessLength = {10, 25, 30};
+		int[] arrayExpectedGraterLength = {10, 25, 30, 40, 100, 0};
+		int[] arrayActualSameLength = Arrays.copyOf(array,  array.length );
+		int[] arrayActualLessLength = Arrays.copyOf(array, array.length -2);
+		int[] arrayActualGraterLength = Arrays.copyOf(array, array.length +1);
+		assertArrayEquals(arrayExpectedSameLength, arrayExpectedSameLength);
+		assertArrayEquals(arrayExpectedLessLength, arrayExpectedLessLength);
+		assertArrayEquals(arrayExpectedGraterLength, arrayExpectedGraterLength);
+	}
+	
+	@Test
+	void arraycopyTest() {
+		int [] array1 = {3, 10, 20, 15};
+		int [] expected = {10, 25, 10, 20, 100};
+		int [] arrayCopy = Arrays.copyOf (array, array.length);
+		System.arraycopy(array1, 1, arrayCopy, 2, 2);
+		assertArrayEquals(expected, arrayCopy);
+	}
+	@Test
+	void binarySearchIntTest() {
+		//TODO
+	}
+	}
+	
+	
+
