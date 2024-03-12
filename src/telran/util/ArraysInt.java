53f6f2eb26b8729d2e7244bc4ad9a3fad30adf53
DmitriyVolchenko <Dmitriy.Volchenko@gmail.com>
2024-03-12 19:50:09 +0200

    adds method removeNumber

--- src/telran/util/ArraysInt.java
+++ src/telran/util/ArraysInt.java
@@ -9,21 +9,19 @@ public class ArraysInt {
 		newArray[newArray.length-1]=number;
 		return newArray;
 	}
-	public static int[] insertNumber(int[] array,int index,  int number) {
-		
-		int[]newArray = Arrays.copyOf(array, array.length+1); //repeating 1
-		
+	public static int[] insertNumber(int[] array,int index,  int number) {		
+		int[]newArray = Arrays.copyOf(array, array.length+1); //repeating 1		
 		System.arraycopy(newArray, index, newArray, index+1, newArray.length - index - 1);
 		newArray[index] = number;
 		
-		
-		
 		return newArray;
 	}
-	public static int[] removeNumber(int[] array,int index) {
-		//TODO TODO apply method arraycopy of class System
-		return null;
-	}
+	
+	 public static int[] removeNumber(int[] array, int index) {
+	        int[] newArray = Arrays.copyOf(array, array.length - 1);
+	        System.arraycopy(array, index + 1, newArray, index, newArray.length - index);
+	        return newArray;
+	 }
 	public static int[] insertSorted(int[] sortedArray,int number) {
 		//TODO insert number into sorted array with keeping the sorted order
 //apply method binerySearch of the class Arrays
